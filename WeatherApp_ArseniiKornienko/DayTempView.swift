cb31e8b519800e2329c16033ccfe7a958a6c81ce
Arsenii K <167592505+Arseny89@users.noreply.github.com>
2024-05-05 00:12:33 -0700

    Created UIViews

---
+++ WeatherApp_ArseniiKornienko/DayTempView.swift
@@ -0,0 +1,124 @@
+//
+//  DayTempView.swift
+//  WeatherApp_ArseniiKornienko
+//
+//  Created by Арсений Корниенко on 5/4/24.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+
+final class DayTempView: UIView {
+    struct InputData {
+        var icon: UIImage?
+        let temp: Int
+        let time: String
+    }
+    
+    private let stackView = UIStackView()
+    private let scrollView = UIScrollView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .systemBlue.withAlphaComponent(0.5)
+        layer.cornerRadius = 12
+        
+        setupScrollView()
+        setupStackView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setup(_ data: [InputData]) {
+        data.enumerated().forEach { index, data in
+            let view = HourWeatherView()
+            view.setup(data)
+            stackView.addArrangedSubview(view)
+            if index == 0 {
+                stackView.setCustomSpacing(10, after: view)
+            }
+        }
+    }
+    
+    private func setupScrollView() {
+        addSubview(scrollView)
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview().inset(5)
+            make.verticalEdges.equalToSuperview().inset(5)
+        }
+    }
+    
+    private func setupStackView() {
+        scrollView.addSubview(stackView)
+        stackView.distribution = .fillEqually
+        stackView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
+
+extension DayTempView {
+    
+    final class HourWeatherView: UIView {
+        
+        private let timeLabel = UILabel()
+        private let icon = UIImageView()
+        private let tempLabel = UILabel()
+        private let stackView = UIStackView()
+        private let scrollView = UIScrollView()
+        
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            
+            setupStackView()
+            setupTimeLabel()
+            setupIcon()
+            setupTemp()
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        func setup(_ data: InputData) {
+            timeLabel.text = data.time
+            icon.image = data.icon
+            tempLabel.text = "\(data.temp)º"
+        }
+        private func setupTimeLabel() {
+            stackView.addArrangedSubview(timeLabel)
+            timeLabel.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+            timeLabel.textColor = .white
+            timeLabel.textAlignment = .center
+        }
+        
+        private func setupIcon() {
+            stackView.addArrangedSubview(icon)
+            icon.contentMode = .scaleAspectFit
+        }
+        
+        private func setupTemp() {
+            stackView.addArrangedSubview(tempLabel)
+            tempLabel.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+            tempLabel.textColor = .white
+            tempLabel.textAlignment = .center
+        }
+        
+        
+        private func setupStackView() {
+            addSubview(stackView)
+            stackView.axis = .vertical
+            stackView.spacing = 10
+            stackView.distribution = .fillEqually
+            
+            stackView.snp.makeConstraints { make in
+                make.edges.equalToSuperview()
+            }
+        }
+    }
+}
---
+++ WeatherApp_ArseniiKornienko/TempRangeView.swift
@@ -0,0 +1,46 @@
+//
+//  TempRangeView.swift
+//  WeatherApp_ArseniiKornienko
+//
+//  Created by Арсений Корниенко on 5/4/24.
+//
+
+import Foundation
+import Foundation
+import UIKit
+import SnapKit
+
+final class TempRangeView: UIView {
+    struct InputData {
+        let minTemp: Double
+        let maxTemp: Double
+        let minDayTemp: Double
+        let maxDayTemp: Double
+        let currentTemt: Double?
+        let icon: UIImage
+    }
+    
+    private let titleLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemBlue.withAlphaComponent(0.5)
+        layer.cornerRadius = 12
+        setupTitleLabel()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupTitleLabel() {
+        addSubview(titleLabel)
+        titleLabel.text = "Прогноз на 10 дней"
+        titleLabel.textColor = .white
+        titleLabel.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(5)
+            make.leading.equalToSuperview().inset(10)
+        }
+    }
+}
