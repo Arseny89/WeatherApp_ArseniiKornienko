876025c97b1562dfb7bcc8f2b3a592634707712f
Arsenii K <167592505+Arseny89@users.noreply.github.com>
2024-06-13 22:08:59 -0700

    Update CurrentWeatherResponse.swift

--- WeatherApp_ArseniiKornienko/Models/CurrentWeatherResponse.swift
+++ WeatherApp_ArseniiKornienko/Models/CurrentWeatherResponse.swift
@@ -7,11 +7,27 @@
 
 import Foundation
 
-struct CurrentWeather: Codable {
-    let name: String
+struct CurrentWeatherResponse: Decodable {
+    let name: String?
+    let id: Int
     let timezone: Int
-    let country: String
+    let sys: Sys
     let coordinates: Coordinates
     let main: WeatherItem
     let weather: [WeatherConditions]
+    enum CodingKeys: String, CodingKey {
+        case name
+        case timezone
+        case coordinates = "coord"
+        case main
+        case weather
+        case sys
+        case id
+    }
+    
+    struct Sys: Codable {
+        let country: String
+        let sunrise: Int
+        let sunset: Int
+    }
 }
