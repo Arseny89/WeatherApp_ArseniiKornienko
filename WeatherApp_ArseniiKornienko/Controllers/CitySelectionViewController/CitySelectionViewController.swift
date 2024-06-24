59ff717a5a58d30280b561eb033d3fd3cc4bd476
Arsenii K <167592505+Arseny89@users.noreply.github.com>
2024-06-24 13:21:23 -0700

    Update CitySelectionViewController.swift

--- WeatherApp_ArseniiKornienko/Controllers/CitySelectionViewController/CitySelectionViewController.swift
+++ WeatherApp_ArseniiKornienko/Controllers/CitySelectionViewController/CitySelectionViewController.swift
@@ -184,12 +184,16 @@ final class CitySelectionViewController: UIViewController {
     
     private func setupDataToPresentedViewController() {
         DispatchQueue.main.async { [self] in
-            if let weatherData = sections.first?.items,
-               let presentedCityWeatherController = presentedViewController as? WeatherViewController,
-               let data = weatherData.first(where: { $0.id == presentedCityWeatherController.cityID }) {
-                
-                presentedCityWeatherController.viewModel.setup(data)
+            guard let weatherData = sections.first?.items,
+                  let presentedCityWeatherController =
+                    presentedViewController as? WeatherViewController,
+                  let data = weatherData.first(where: {
+                      $0.id == presentedCityWeatherController.cityID
+                  }) else {
+                return
             }
+            
+            presentedCityWeatherController.viewModel.setup(data)            
         }
     }
     
